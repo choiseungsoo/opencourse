454d4c9bebce226b93b59b99546ad2dc7d46e8c9
CHOI SEUNG SOO <choi.ss.essoft@gmail.com>
2019-06-29 11:37:55 +0900

    Add files via upload

--- common.js
+++ common.js
@@ -3,5 +3,15 @@ function alert4(){
 }
 
 function alertOnLoad(){
-	confirm('this page is loaded!!');	
+	console.log('alertOnLoad');
+	alert('경고');
+	
+	var rtnValue = confirm("body에 글을 쓰시겠습니까?");
+	
+	if( rtnValue == true)
+	{  
+		document.write('글이 써졌어요');	
+	} else {  
+		return false;
+	}
 }
